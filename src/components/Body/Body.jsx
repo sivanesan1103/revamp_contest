a4df661d2755325d05e87eaea2dbac9c0d4b4384
sivaofficials <sivaoofficial03@gmail.com>
2024-09-03 10:48:02 +0530

    color change

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,20 +1,21 @@
 import Image from './girl.png'
 export const Body = () => {
     return (
-        <div className='flex justify-center h-screen p-8 bg-[#B8DDE3]'>
-            <div className='w-1/2 mt-24 text-center'>
-                <p className='mb-3 text-6xl font-extrabold'>Start your
-                Own Online Learning</p>
-                <p className='mb-2 text-lg font-bold'>Unlimited access to all 60+ instructors.</p>
-                <p>You`re guaranteed to find something that`s right for you.</p>
-            </div>
-            <div className='w-1/2'>
-                <img 
-                    src={Image} 
-                    alt="Cover" 
-                    className="object-cover w-full h-full mr-20"
-                />
-            </div>
-        </div>
+<div className='flex flex-col md:flex-row justify-center h-screen p-8 bg-[#B8DDE3]'>
+    <div className='mt-12 text-center md:w-1/2 md:mt-24'>
+        <p className='mb-3 text-4xl font-extrabold md:text-6xl'>Start your Own Online Learning</p>
+        <p className='mb-2 font-bold text-md md:text-lg'>Unlimited access to all 60+ instructors.</p>
+        <p className='text-sm md:text-base'>You're guaranteed to find something that’s right for you.</p>
+    </div>
+    <div className='h-64 mt-5 md:h-full md:mt-0'>
+        <img 
+            src={Image} 
+            alt="Cover" 
+            className="object-contain w-full h-full"
+        />
+    </div>
+</div>
+
+
     );
 };
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -80,10 +80,10 @@ export const Navbar = () => {
 
                 {/* Main Menu */}
                 <div className={`${isDropdown ? 'block' : 'hidden'} w-full md:block md:w-auto`}>
-                    <ul className="flex flex-col p-4 mt-4 font-medium border border-gray-100 rounded-lg md:p-0 bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                    <ul className="flex flex-col p-4 mt-4 font-medium bg-transparent border border-gray-100 rounded-lg md:p-0 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0">
                         {/* Menu Items */}
                         <li>
-                            <a href="#" className="block px-3 py-2 text-white rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent" aria-current="page">
+                            <a href="#" className="block px-3 py-2 rounded md:bg-transparent md:text-black md:p-0 " aria-current="page">
                                 Home
                             </a>
                         </li>
