51aa11479856c3fdf40ce522d9421a3336ad7433
sivaofficials <sivaoofficial03@gmail.com>
2024-09-05 12:53:12 +0530

    card page namee chang

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -1,18 +1,27 @@
 import { useRouteError } from "react-router-dom";
+import errvid from './Vadivelu_empty_pocket.mp4';
 
 export const ErrorPage = () => {
     let error = useRouteError();
     console.log(error);
-    
-    return (
-        <div>
-            <h1>Error</h1>
 
-                <div>
-                    <p>Status: {error.status}</p>
-                    
-                </div>
-            
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen text-gray-900 bg-gray-100">
+            <h1 className="mb-4 text-4xl font-bold">Error</h1>
+            <p className="mb-4 text-lg">Status: {error.status || 'Unknown'}</p>
+            <div className="w-full max-w-md">
+            <video
+    controls
+    src={errvid}
+    className="w-full"
+    autoPlay
+    loop
+    muted
+    playsInline
+>
+                    Your browser does not support the video tag.
+                </video>
+            </div>
         </div>
     );
 };
